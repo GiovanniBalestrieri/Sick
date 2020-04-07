b072d739747a3fde4e2b5cc88994c20587da5ae5
Giovanni <giovanni.balestrieri@inria.fr>
2020-04-07 13:45:30 +0200

    Removed GeoDecoder. Google might save stuff on its server. Probably does anyway

--- app/src/main/java/com/google/android/gms/location/sample/locationaddress/FetchAddressIntentService.java
+++ app/src/main/java/com/google/android/gms/location/sample/locationaddress/FetchAddressIntentService.java
@@ -27,7 +27,10 @@ import android.text.TextUtils;
 import android.util.Log;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -86,74 +89,18 @@ public class FetchAddressIntentService extends IntentService {
             return;
         }
 
-        // Errors could still arise from using the Geocoder (for example, if there is no
-        // connectivity, or if the Geocoder is given illegal location data). Or, the Geocoder may
-        // simply not have an address for a location. In all these cases, we communicate with the
-        // receiver using a resultCode indicating failure. If an address is found, we use a
-        // resultCode indicating success.
+        StringBuilder bundleLocation  = new StringBuilder(100);
+        bundleLocation.append(Double.toString(location.getLongitude()));
+        bundleLocation.append(",");
+        bundleLocation.append(Double.toString(location.getLatitude()));
+        bundleLocation.append(",");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
+        String data = simpleDateFormat.format(new Date());
+        bundleLocation.append(data);
 
-        // The Geocoder used in this sample. The Geocoder's responses are localized for the given
-        // Locale, which represents a specific geographical or linguistic region. Locales are used
-        // to alter the presentation of information such as numbers or dates to suit the conventions
-        // in the region they describe.
-        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        Log.d("AAA",bundleLocation.toString());
 
-        // Address found using the Geocoder.
-        List<Address> addresses = null;
-        String bundleLocation  = "";
-        try {
-            // Using getFromLocation() returns an array of Addresses for the area immediately
-            // surrounding the given latitude and longitude. The results are a best guess and are
-            // not guaranteed to be accurate.
-            addresses = geocoder.getFromLocation(
-                    location.getLatitude(),
-                    location.getLongitude(),
-                    // In this sample, we get just a single address.
-                    1);
-        } catch (IOException ioException) {
-            // Catch network or other I/O problems.
-            errorMessage = getString(R.string.service_not_available);
-            Log.e(TAG, errorMessage, ioException);
-        } catch (IllegalArgumentException illegalArgumentException) {
-            // Catch invalid latitude or longitude values.
-            errorMessage = getString(R.string.invalid_lat_long_used);
-            Log.e(TAG, errorMessage + ". " +
-                    "Latitude = " + location.getLatitude() +
-                    ", Longitude = " + location.getLongitude(), illegalArgumentException);
-        }
-
-        // Handle case where no address was found.
-        if (addresses == null || addresses.size()  == 0) {
-            if (errorMessage.isEmpty()) {
-                errorMessage = getString(R.string.no_address_found);
-                Log.e(TAG, errorMessage);
-            }
-            deliverResultToReceiver(Constants.FAILURE_RESULT, errorMessage);
-        } else {
-            Address address = addresses.get(0);
-            ArrayList<String> addressFragments = new ArrayList<>();
-
-            // Fetch the address lines using {@code getAddressLine},
-            // join them, and send them to the thread. The {@link android.location.address}
-            // class provides other options for fetching address details that you may prefer
-            // to use. Here are some examples:
-            // getLocality() ("Mountain View", for example)
-            // getAdminArea() ("CA", for example)
-            // getPostalCode() ("94043", for example)
-            // getCountryCode() ("US", for example)
-            // getCountryName() ("United States", for example)
-            for(int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
-                addressFragments.add(address.getAddressLine(i));
-            }
-
-
-            addressFragments.add(Double.toString(location.getLatitude()));
-            //addressFragments.add(location.getLatitude());
-            //        location.getLongitude(),);
-            Log.i(TAG, getString(R.string.address_found));
-            deliverResultToReceiver(Constants.SUCCESS_RESULT,
-                    TextUtils.join(System.getProperty("line.separator"), addressFragments));
-        }
+        deliverResultToReceiver(Constants.SUCCESS_RESULT,bundleLocation.toString());
     }
 
     /**
